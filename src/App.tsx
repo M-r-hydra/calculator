affe0cfd70509c116860e74df635ce1d991507e4
hydra <me74242@gmail.com>
2022-12-07 19:03:04 +0330

    styles end

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App = () => {
       <button>1</button>
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
@@ -26,7 +27,7 @@ const App = () => {
       <button>8</button>
       <button>9</button>
       <button>-</button>
-      <button>-</button>
+      <button>.</button>
       <button>0</button>
       <button className="span-two">=</button>
     </div>
